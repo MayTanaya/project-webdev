aa11138f347784ddcfab228ba85f3bdfd50257af
MayTanaya <hasnamaytanaya@gmail.com>
2025-03-18 12:27:54 +0700

    tambah about us

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     
 </head>
 <body>
-
+<!-- Start navbar -->
     <nav class="navbar navbar-expand-lg navbar-custom">
         <div class="container">
             <!-- Logo -->
@@ -42,12 +42,66 @@
 
                 <!-- Login & Sign Up -->
                 <form class="d-flex">
-                    <button class="btn btn-login me-2" type="button">Login</button>
+                    <button class="btn btn-login me-2" type="button" href="form-rev.html">Login</button>
                     <button class="btn btn-signup" type="button">Sign Up</button>
                 </form>
             </div>
         </div>
     </nav>
+<!-- End Navbar -->
+
+<!-- Hero Section -->
+ <div class="container-fluid hero d-flex align-items-center justify-content-center text-center">
+    <div class="formcontainer">
+        <div class="row d-flex justify-content-center text-center">
+        <!-- text -->
+        <div>
+            <h1 class="hero-title">
+                Happiness comes from <br> your action
+            </h1>
+        <!-- button -->
+            <button class="btn btn-dark px-5 py-3 fw-bold mt-4" type="button">Donate Now</button>
+        </div>
+        </div>
+    </div>
+</div>
+<!-- End hero section -->
+
+<!-- Start About Us -->
+<section id="about" class="about-section-padding">
+    <div class="container">
+        <div class="row d-flex align-items-center">
+            <div class="col-lg-6 col-md-12">
+                <div class="about-img">
+                    <img src="images/donate.jpg" alt="our mission" class="img-fluid">
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-12">
+                <div class="about-text">
+                        <h2> Our Mission </h2>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus fermentum mi, a consectetur ligula dapibus eu. 
+                        Quisque pulvinar erat sed odio molestie scelerisque. Ut efficitur vehicula enim vitae aliquet. Nam dignissim elit varius 
+                        venenatis condimentum. Etiam volutpat sit amet quam dapibus dictum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+                        per inceptos himenaeos. Maecenas egestas pulvinar arcu, a faucibus dolor volutpat vel. Mauris sed neque bibendum, tempus nibh quis, 
+                        bibendum ligula. Vestibulum malesuada urna nec orci dictum, eget interdum lacus sagittis.
+                        </p>
+                </div>
+            </div>
+        </div>
+    </div>
+ </section>
+<!-- End About Us -->
+
+<!-- Start Open Donations -->
+<section>
+    <div class="open-donate-text">
+        <h2 class="open-donations"> Open Donations </h2>
+    </div>
+</section>
+<!-- End Open Donations -->
+
+<!-- Start Open Documentation -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
